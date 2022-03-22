0eee55fe128dfd9a34de01fd0f8a59afa0fff503
OlivierBillant <99239307+OlivierBillant@users.noreply.github.com>
2022-03-22 11:43:56 +0100

    Proposition v1 fonctionnelle

--- src/controller/AppliTest.java
+++ src/controller/AppliTest.java
@@ -1,22 +1,44 @@
 package controller;
 
-import com.mysql.jdbc.Connection;
-
+import model.bll.CatalogueManager;
 import model.bo.Livre;
-import model.dal.Connexion;
-import model.dal.LivreDAO;
 
 public class AppliTest {
 
 	public static void main(String[] args) {
-//		java.sql.Connection cnx = Connexion.getCnx();
+		CatalogueManager catalogue = new CatalogueManager();
 		Livre livre1 = new Livre("La communaute de l'anneau","123-456-789" ,"J.R.R. Tolkien");
 		Livre livre2 = new Livre("Les deux tours","123-456-789" ,"J.R.R. Tolkien");
 		Livre livre3 = new Livre("Le retour du roi","123-456-789" ,"J.R.R. Tolkien");
-		LivreDAO bibliotheque = new LivreDAO();
-		bibliotheque.insert(livre1);
-		bibliotheque.insert(livre2);
-		bibliotheque.insert(livre3);
+		Livre livre4 = new Livre("Le Silmarillon","666-666-666" ,"J.R.R. Tolkien");
+		
+		System.out.println("\nAjout au catalogue  : " );
+		catalogue.addLivre(livre1);
+		catalogue.addLivre(livre2);
+		catalogue.addLivre(livre3);
+		catalogue.addLivre(livre4);
+		
+		System.out.println("\nAffichage du catalogue  : " );
+		catalogue.getCatalogue();
+		
+		System.out.println("\nAffichage d'un seul livre  : " );
+		catalogue.selectLivre(1);
+		
+		System.out.println("\nSuppression d'un livre  : " );
+		catalogue.enleverLivre(4);
+		catalogue.getCatalogue();
+		
+		System.out.println("\nModification d'un livre  : " );
+		System.out.println("\nLivres avant modification : ");
+		System.out.println(livre2.toString());
+		System.out.println(livre3.toString());
+		livre2.setIsbn("321-654-987");
+		livre3.setIsbn("147-258-369");
+		System.out.println("\nLivres après modification  : " );
+		catalogue.modifierLivre(livre2);
+		catalogue.modifierLivre(livre3);
+		catalogue.selectLivre(2);
+		catalogue.selectLivre(3);
+		}
 	}
 
-}
--- src/model/bll/CatalogueManager.java
+++ src/model/bll/CatalogueManager.java
@@ -1,5 +1,39 @@
 package model.bll;
 
+import java.util.List;
+
+import model.bo.Livre;
+import model.dal.LivreDAO;
+
 public class CatalogueManager {
 
-}
+	public void getCatalogue() {
+		LivreDAO bibliotheque = new LivreDAO();
+		List<Livre> stock = bibliotheque.selectAll();
+		for (Livre livre : stock) {
+			System.out.println(livre);
+		}
+		System.out.println("");
+	}
+	
+	public void addLivre(Livre l) {
+		LivreDAO bibliotheque = new LivreDAO();
+		bibliotheque.insert(l);
+	}
+
+	public void selectLivre(int id) {
+		LivreDAO bibliotheque = new LivreDAO();
+		System.out.println(bibliotheque.selectBy(id));
+
+	}
+	
+	public void enleverLivre(int id) {
+		LivreDAO bibliotheque = new LivreDAO();
+		bibliotheque.delete(id);
+	}
+	
+	public void modifierLivre(Livre l) {
+		LivreDAO bibliotheque = new LivreDAO();
+		bibliotheque.update(l);
+	}
+}	
