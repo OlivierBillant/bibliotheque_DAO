87088e9f92463c36f18f90621ef8697f769175b7
OlivierBillant <99239307+OlivierBillant@users.noreply.github.com>
2022-03-29 09:33:03 +0200

    Petite maj restante

--- src/controller/AppliTest.java
+++ src/controller/AppliTest.java
@@ -14,42 +14,45 @@ import model.dal.LivreDAO;
 public class AppliTest {
 
 	public static void main(String[] args) {
-//		CatalogueManager catalogue = new CatalogueManager();
-//		Livre livre1 = new Livre("La communaute de l'anneau","123-456-789" ,"J.R.R. Tolkien");
-//		Livre livre2 = new Livre("Les deux tours","123-456-789" ,"J.R.R. Tolkien");
-//		Livre livre3 = new Livre("Le retour du roi","123-456-789" ,"J.R.R. Tolkien");
-//		Livre livre4 = new Livre("Le Silmarillon","666-666-666" ,"J.R.R. Tolkien");
-//		
-//		System.out.println("\nTest connexion et ajout catalogue  : " );
-//		CatalogueManager.addLivre(livre1);
-//		CatalogueManager.addLivre(livre2);
-//		CatalogueManager.addLivre(livre3);
-//		CatalogueManager.addLivre(livre4);
-//		
-//		System.out.println("\nAffichage du catalogue  : " );
-//		CatalogueManager.getCatalogue();
-//		
-//		System.out.println("\nAffichage d'un seul livre  : " );
-//		CatalogueManager.selectLivre(1);
-//		
-//		System.out.println("\nSuppression d'un livre  : " );
-//		CatalogueManager.enleverLivre(4);
-//		CatalogueManager.getCatalogue();
-//		
-//		System.out.println("\nModification d'un livre  : " );
-//		System.out.println("\nLivres avant modification : ");
-//		System.out.println(livre2.toString());
-//		System.out.println(livre3.toString());
-//		livre2.setIsbn("321-654-987");
-//		livre3.setIsbn("147-258-369");
-//		System.out.println("\nLivres après modification  : " );
-//		CatalogueManager.modifierLivre(livre2);
-//		CatalogueManager.modifierLivre(livre3);
-//		CatalogueManager.selectLivre(2);
-//		CatalogueManager.selectLivre(3);
-//		
-//		CatalogueManager.bruleAlexandrie();
-//		CatalogueManager.constructionAlexandrie();
+		CatalogueManager catalogue = new CatalogueManager();
+		Livre livre1 = new Livre("La communaute de l'anneau","123-456-789" ,"J.R.R. Tolkien");
+		Livre livre2 = new Livre("Les deux tours","123-456-789" ,"J.R.R. Tolkien");
+		Livre livre3 = new Livre("Le retour du roi","123-456-789" ,"J.R.R. Tolkien");
+		Livre livre4 = new Livre("Le Silmarillon","666-666-666" ,"J.R.R. Tolkien");
+		
+		System.out.println("\nTest connexion et ajout catalogue  : " );
+		CatalogueManager.addLivre(livre1);
+		CatalogueManager.addLivre(livre2);
+		CatalogueManager.addLivre(livre3);
+		CatalogueManager.addLivre(livre4);
+		
+		System.out.println("\nAffichage du catalogue  : " );
+		CatalogueManager.getCatalogue();
+		
+		System.out.println("\nAffichage d'un seul livre  : " );
+		CatalogueManager.selectLivre(1);
+		
+		System.out.println("\nSuppression d'un livre  : " );
+		CatalogueManager.enleverLivre(4);
+		CatalogueManager.getCatalogue();
+		
+		System.out.println("\nModification d'un livre  : " );
+		System.out.println("\nLivres avant modification : ");
+		System.out.println(livre2.toString());
+		System.out.println(livre3.toString());
+		livre2.setIsbn("321-654-987");
+		livre3.setIsbn("147-258-369");
+		System.out.println("\nLivres après modification  : " );
+		CatalogueManager.modifierLivre(livre2);
+		CatalogueManager.modifierLivre(livre3);
+		CatalogueManager.selectLivre(2);
+		CatalogueManager.selectLivre(3);
+		
+		CatalogueManager.bruleAlexandrie();
+		CatalogueManager.constructionAlexandrie();
+	}
+}
+		/* Test import csv vian scanner. Fonctionnel final dans Dataset
 		Scanner sc;
 		Dataset dataset = new Dataset();
 		Livre livre = null;
@@ -58,7 +61,7 @@ public class AppliTest {
 		
 		try {
 			sc = new Scanner(new File("import.csv"));
-//			sc.useDelimiter(",");   //sets the delimiter pattern  
+			sc.useDelimiter(",");   //sets the delimiter pattern  
 			while (sc.hasNext())  //returns a boolean value  
 			{  
 				temp = sc.next().split(",");
@@ -81,7 +84,6 @@ public class AppliTest {
 			e.printStackTrace();
 		}
 		}	
-	
-	
 	}
+*/	
 
